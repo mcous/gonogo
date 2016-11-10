d763a8c896e6701ce5e0f4fae79f1cd0c3a73ca8
Mike Cousins <mike@cousins.io>
2016-11-10 00:43:34 -0500

    Add basic object schema functionality

--- index.js
+++ index.js
@@ -4,5 +4,19 @@
 const assert = require('assert')
 
 module.exports = function gonogo (schema) {
-  return (target) => assert(schema(target), `value > ${target} < failed validation function`)
+  if (typeof schema === 'function') {
+    return (target) => {
+      assert(schema(target), `gonogo: value > ${target} < failed validation function`)
+    }
+  }
+
+  assert(
+    typeof schema === 'object',
+    `gonogo: USAGE: expected function or object schema, found > ${schema} <`)
+
+  const schemaKeys = Object.keys(schema)
+
+  return (target) => schemaKeys.forEach((key) => {
+    assert(schema[key](target[key]), `gonogo: field '${key}' failed validation function`)
+  })
 }
--- test/gonogo_test.js
+++ test/gonogo_test.js
@@ -7,17 +7,44 @@ const it = require('mocha').it
 const gng = require('../')
 
 describe('gonogo', function () {
+  const pass = (value) => value === 'pass'
+  const fail = (value) => value !== 'pass'
+
+  it('should throw if the schema is not a function nor object', function () {
+    assert.throws(() => gng(1), /expected function or object schema/)
+    assert.throws(() => gng(false), /expected function or object schema/)
+    assert.throws(() => gng(''), /expected function or object schema/)
+  })
+
   it('should not throw if a target passes a validation function', function () {
-    const schema = () => true
+    const schema = pass
     const validate = gng(schema)
 
-    validate(null)
+    validate('pass')
   })
 
   it('should throw if a target does not pass a validation function', function () {
-    const schema = () => false
+    const schema = fail
     const validate = gng(schema)
 
-    assert.throws(() => validate(null), /value > null < failed validation function/)
+    assert.throws(
+      () => validate('pass'),
+      /value > pass < failed validation function/)
+  })
+
+  it('should not throw if a target passes validation functions for all its keys', function () {
+    const schema = {foo: pass, bar: pass, baz: pass}
+    const validate = gng(schema)
+
+    validate({foo: 'pass', bar: 'pass', baz: 'pass'})
+  })
+
+  it('should throw if a target fails validation functions for any of its keys', function () {
+    const schema = {foo: pass, bar: fail, baz: pass}
+    const validate = gng(schema)
+
+    assert.throws(
+      () => validate({foo: 'pass', bar: 'pass', baz: 'pass'}),
+      /field 'bar' failed validation function/)
   })
 })
